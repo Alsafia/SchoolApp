09eb9739a71d4e09fcf65332f85f291398715b91
Alsafia <79151685+Alsafia@users.noreply.github.com>
2026-07-01 21:22:09 +0000

    Tagreeb

--- artifacts/school-app/src/main.tsx
+++ artifacts/school-app/src/main.tsx
@@ -1,28 +1,5 @@
-import { StrictMode } from "react";
-import { Toaster } from "sonner"; // أضف هذا فوق
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
 
-import "./styles.css";
-
-//const router = getRouter();
-const router = getRouter();
-
-console.log("ROUTER:", router);
-
-const rootElement = document.getElementById("root")!;
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-
-
-    <Toaster
-      position="top-center"
-      toastOptions={{
-        className: "custom-toast",
-      }}
-    />
-  </StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <div style={{ color: "black" }}>HELLO WORKS</div>
 );
