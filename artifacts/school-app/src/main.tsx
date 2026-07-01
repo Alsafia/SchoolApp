7494be55aaf877e2a2d82b9b6bd904a26308ac41
Alsafia <79151685+Alsafia@users.noreply.github.com>
2026-07-01 21:27:54 +0000

    test app render

--- artifacts/school-app/src/main.tsx
+++ artifacts/school-app/src/main.tsx
@@ -1,5 +1,3 @@
-import { createRoot } from "react-dom/client";
-
 createRoot(document.getElementById("root")!).render(
-  <div style={{ color: "black" }}>HELLO WORKS</div>
+  <RouterProvider router={router} />
 );
